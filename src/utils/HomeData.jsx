517bb28343670b73e41b0aecdaa16e68a5f26299
Wasim <princewasim971@gmail.com>
2025-01-15 22:13:36 -0800

    resolve error

---
+++ src/utils/HomeData.jsx
@@ -0,0 +1,352 @@
+import CosmicLogo from '../assets/CosmicLogo.png'
+import banner1 from  '../assets/Images/ConstructionofMasjid/const1_(1).jpg'
+
+export const HomeData = {
+    banners: [
+    
+        {
+            image: 'https://images.unsplash.com/photo-1482731215275-a1f151646268?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fEV4cGVydGlzZSUyMFJlZGVmaW5lZCUyMGNvbnN0cnVjdGlvbiUyMHBpY3R1cmV8ZW58MHx8MHx8fDA%3D',
+            title: 'Expertise Redefined',
+            description: 'Specializing in Civil, Electrical, and Mechanical Projects',
+            buttonText: 'LEARN MORE',
+            link: '#projects'
+        },
+        {
+            image: 'https://plus.unsplash.com/premium_photo-1684450118907-ee3e684c3455?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            title: 'Innovative Solutions',
+            description: 'Delivering Excellence in Construction and Infrastructure',
+            buttonText: 'EXPLORE MORE',
+            link: '#services'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1485083269755-a7b559a4fe5e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8VGltZWx5JTIwRGVsaXZlcnklMjBjb25zdHJ1Y3Rpb24lMjBwaWN0dXJlfGVufDB8fDB8fHww',
+            title: 'Timely Delivery',
+            description: 'Redefining Project Completion Standards Across Industries',
+            buttonText: 'OUR WORK',
+            link: '#portfolio'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1495036019936-220b29b930ea?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            title: 'Quality Commitment',
+            description: '18 Years of Professional Excellence and Distinction',
+            buttonText: 'DISCOVER MORE',
+            link: '#about'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1504307651254-35680f356dfd?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            title: 'Sustainable Growth',
+            description: 'Leading the Way in Housing, Roads, and Environmental Projects',
+            buttonText: 'SEE MORE',
+            link: '#sustainability'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1508450859948-4e04fabaa4ea?q=80&w=1379&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            title: 'Crafting Futures',
+            description: 'Trusted Partners in Industrial and Commercial Construction',
+            buttonText: 'GET STARTED',
+            link: '#contact'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1553946550-4b8f3eea5451?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDI1fHx8ZW58MHx8fHx8',
+            title: 'Ahead of Time',
+            description: 'Proven Track Record in Delivering Projects Ahead of Schedule',
+            buttonText: 'KNOW MORE',
+            link: '#testimonials'
+        },
+        {
+            image: 'https://plus.unsplash.com/premium_photo-1681823639539-8459a61eefbe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8',
+            title: 'Proven Success',
+            description: 'Proven Track Record in Delivering Projects Ahead of Schedule',
+            buttonText: 'KNOW MORE',
+            link: '#testimonials'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1517089596392-fb9a9033e05b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fEV4Y2VsbGVuY2UlMjBEZWxpdmVyZWQlMjBjb25zdHJ1Y3Rpb24lMjBwaWN0dXJlfGVufDB8fDB8fHww',
+            title: 'Excellence Delivered',
+            description: 'Proven Track Record in Delivering Projects Ahead of Schedule',
+            buttonText: 'KNOW MORE',
+            link: '#testimonials'
+        },
+        {
+            image: 'https://media.istockphoto.com/id/2185195503/photo/team-of-engineers-supervising-modern-manufacturing-facility.webp?a=1&b=1&s=612x612&w=0&k=20&c=yaR91lZREBo-qeBxmqwTQqCq9Pa56nKIQ0-3zI9mzy4=',
+            title: 'Trusted Partner',
+            description: 'Proven Track Record in Delivering Projects Ahead of Schedule',
+            buttonText: 'KNOW MORE',
+            link: '#testimonials'
+        },
+        {
+            image: 'https://plus.unsplash.com/premium_photo-1661335283480-4ec3eae8d313?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8RGVzaWduJTIwRXhwZXJ0cyUyMGNvbnN0cnVjdGlvbiUyMHBpY3R1cmV8ZW58MHx8MHx8fDA%3D',
+            title: 'Design Experts',
+            description: 'Specialists in Infrastructure, Design, and Environmental Areas',
+            buttonText: 'VIEW DETAILS',
+            link: '#design'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1509453721491-c3af5961df76?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fFlvdXIlMjBWaXNpb24lMjBjb25zdHJ1Y3Rpb24lMjBwaWN0dXJlfGVufDB8fDB8fHww',
+            title: 'Your Vision',
+            description: 'Creating Landmarks Through Precision and Dedication',
+            buttonText: 'PARTNER WITH US',
+            link: '#partners'
+        },
+    ],      
+    testimonials: [
+        {
+            image: 'https://construm.bugfinder.net/assets/uploads/content/63550eaeaab481666518702.jpg',
+            text: 'Working with Cosmic Construction Company was an absolute pleasure...',
+            name: 'Adam Young',
+            title: 'Business Analyst'
+        },
+        {
+            image: 'https://construm.bugfinder.net/assets/uploads/content/63550ecc4b2a41666518732.jpg',
+            text: 'Cosmic Construction Company transformed our outdated home...',
+            name: 'Tom Latham',
+            title: 'Business Analyst'
+        },
+        {
+            image: 'https://construm.bugfinder.net/assets/uploads/content/63550ee2338bd1666518754.jpg',
+            text: 'We chose Cosmic Construction Company for our commercial development...',
+            name: 'Harry Jonas',
+            title: 'Business Analyst'
+        }
+    ],
+    
+    teamMembers: [
+        {
+            name: 'Harry Sivan',
+            designation: 'Managing Director',
+            image: 'https://construm.bugfinder.net/assets/uploads/content/635517cfd163f1666521039.jpg',
+            facebook: 'https://www.facebook.com/',
+            twitter: 'https://twitter.com/',
+            linkedin: 'https://www.linkedin.com/'
+        },
+        {
+            name: 'Troye Sivan',
+            designation: 'Managing Director',
+            image: 'https://construm.bugfinder.net/assets/uploads/content/635517c29dbb91666521026.jpg',
+            facebook: 'https://www.facebook.com/',
+            twitter: 'https://twitter.com/',
+            linkedin: 'https://www.linkedin.com/'
+        },
+        {
+            name: 'Tom Latham',
+            designation: 'Managing Director',
+            image: 'https://construm.bugfinder.net/assets/uploads/content/635517a4de7261666520996.jpg',
+            facebook: 'https://www.facebook.com/',
+            twitter: 'https://twitter.com/',
+            linkedin: 'https://www.linkedin.com/'
+        }
+    ],
+    services : [
+        {
+            title: 'Building Renovation',
+            description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout...',
+            image: 'https://construm.bugfinder.net/assets/uploads/services/635cc0543c6251667022932.png',
+            link: '#'
+        },
+        {
+            title: 'Building Construction',
+            description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout...',
+            image: 'https://construm.bugfinder.net/assets/uploads/services/635cc00cb98791667022860.png',
+            link: '#'
+        },
+        {
+            title: 'Flooring & Roofing',
+            description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout...',
+            image: 'https://construm.bugfinder.net/assets/uploads/services/635cbf29394dd1667022633.png',
+            link: '#'
+        },
+        {
+            title: 'Building Maintenance',
+            description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout...',
+            image: 'https://construm.bugfinder.net/assets/uploads/services/635cbca81b0de1667021992.png',
+            link: '#'
+        },
+        {
+            title: 'Architecture Design',
+            description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout...',
+            image: 'https://construm.bugfinder.net/assets/uploads/services/635cbc022e75a1667021826.png',
+            link: '#'
+        },
+        {
+            title: 'Interior Design',
+            description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout...',
+            image: 'https://construm.bugfinder.net/assets/uploads/services/635cb8b54068f1667020981.png',
+            link: '#'
+        },
+        {
+            title: 'Construction',
+            description: 'Comprehensive construction services, including planning, execution, and maintenance for various projects.',
+            image: 'https://images.unsplash.com/photo-1600585154340-be6161fd9600?crop=entropy&q=80&w=1374&h=800&fit=crop',
+            link: '#'
+        },
+        {
+            title: 'Real Estate',
+            description: 'Expert real estate solutions for residential, commercial, and industrial properties.',
+            image: 'https://images.unsplash.com/photo-1600585154340-be6161fd9600?crop=entropy&q=80&w=1470',
+            link: '#'
+        },
+        {
+            title: 'Project Management Services',
+            description: 'Efficient project management solutions tailored to your construction needs.',
+            image: 'https://images.unsplash.com/photo-1506765515384-028b60a970df?crop=entropy&q=80&w=1470',
+            link: '#'
+        },
+        {
+            title: 'Commercial & Residential Construction',
+            description: 'Top-notch construction services for commercial and residential properties.',
+            image: 'https://images.unsplash.com/photo-1591183694655-21e6af4e899a?crop=entropy&q=80&w=1470',
+            link: '#'
+        },
+        {
+            title: 'Engineering Services',
+            description: 'Professional engineering services for structural, mechanical, and electrical systems.',
+            image: 'https://images.unsplash.com/photo-1517080598410-1baabafd6e09?crop=entropy&q=80&w=1470',
+            link: '#'
+        },
+        {
+            title: 'Procurement Services',
+            description: 'Efficient procurement solutions for sourcing construction materials and equipment.',
+            image: 'https://images.unsplash.com/photo-1517520287167-4bbf64a00d66?crop=entropy&q=80&w=1470',
+            link: '#'
+        },
+        {
+            title: 'Civil Construction Services',
+            description: 'Reliable civil construction services for infrastructure development, roads, bridges, and more.',
+            image: 'https://images.unsplash.com/photo-1563201108-11a25cbe7634?crop=entropy&q=80&w=1470',
+            link: '#'
+        },
+        {
+            title: 'Mechanical Fabrication Services',
+            description: 'Specialized mechanical fabrication services for construction and industrial projects.',
+            image: 'https://images.unsplash.com/photo-1516015670159-d717d9974707?crop=entropy&q=80&w=1470',
+            link: '#'
+        },
+        {
+            title: 'Mechanical Erection Services',
+            description: 'Professional mechanical erection services for industrial and commercial projects.',
+            image: 'https://images.unsplash.com/photo-1517185204-d6c0f12d3881?crop=entropy&q=80&w=1470',
+            link: '#'
+        }
+    ],
+    aboutContent: `
+        At, we believe that every building is a work of art. 
+        Founded with a vision to revolutionize the construction industry, we have grown into a leading provider 
+        of comprehensive construction services, renowned for our commitment to quality, innovation, and customer satisfaction.
+
+        Our team of skilled professionals brings decades of experience to every project, ensuring that we deliver not only structures 
+        but also value and trust. Whether it’s residential, commercial, or industrial construction, we approach each project with a focus 
+        on precision, sustainability, and efficiency.
+
+        From the initial planning stages to the final finishing touches, Cosmic Construction Company is dedicated to creating spaces that 
+        inspire and endure. Our reputation is built on our ability to consistently meet and exceed client expectations, while staying true 
+        to our core values of integrity, excellence, and innovation.
+
+        Join us on a journey to create the future, one building at a time.
+    `,
+    blogs: [
+        {
+            image: "https://construm.bugfinder.net/assets/uploads/blog/6363b62f27c6d1667479087.jpg",
+            author: "Admin",
+            date: "03 Nov, 2022",
+            title: "Don't miss this opportunity to innovate your business",
+            link: "#"
+        },
+        {
+            image: "https://construm.bugfinder.net/assets/uploads/blog/6363a6f0d64391667475184.jpg",
+            author: "Admin",
+            date: "03 Nov, 2022",
+            title: "Don't miss this opportunity to innovate your business",
+            link: "#"
+        },
+        {
+            image: "https://construm.bugfinder.net/assets/uploads/blog/63638de807c251667468776.jpg",
+            author: "Admin",
+            date: "03 Nov, 2022",
+            title: "Don't miss this opportunity to innovate your business",
+            link: "#"
+        }
+    ],
+    logos: [
+        CosmicLogo,
+        CosmicLogo,
+        CosmicLogo,
+        CosmicLogo
+    ],
+    pricingPlans: [
+        {
+            name: 'Basic Plan',
+            price: '50',
+            features: ['Plan Sketching', '8 Month Fixed Price', 'Marketing strategy', 'Webmail Access', 'Outstanding Support', '24/7 customer help desk'],
+            highlighted: false,
+        },
+        {
+            name: 'Standard Plan',
+            price: '100',
+            features: ['Plan Sketching', '8 Month Fixed Price', 'Marketing strategy', 'Webmail Access', 'Outstanding Support', '24/7 customer help desk'],
+            highlighted: true,
+        },
+        {
+            name: 'Premium Plan',
+            price: '150',
+            features: ['Plan Sketching', '8 Month Fixed Price', 'Marketing strategy', 'Webmail Access', 'Outstanding Support', '24/7 customer help desk'],
+            highlighted: false,
+        }
+    ],
+    portfolioItems: [
+        {
+            image: 'https://plus.unsplash.com/premium_photo-1674676471622-feaf2130fc73?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1645421560391-a76d0e77bf84?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1649083048770-82e8ffd80431?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1698824331198-e5ee4ceba86e?q=80&w=1396&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1645931467048-7050c2d1a3e0?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        },
+        {
+            image: 'https://construm.bugfinder.net/assets/uploads/portfolio/6360c21f0634f1667285535.jpg',
+        }
+    ],
+    categories: [
+        'All works',
+        'Residential',
+        'Commercial',
+        'Education',
+        'Office',
+        'Reconstruction',
+        'Hospital',
+        'Construction',
+        'Reconstrucción'
+    ],
+    features: [
+        {
+            image: 'https://construm.bugfinder.net/assets/uploads/content/6354e0181f97c1666506776.png',
+            title: 'Unique Technology',
+        },
+        {
+            image: 'https://construm.bugfinder.net/assets/uploads/content/6354e0251200d1666506789.png',
+            title: 'Great Support',
+        },
+        {
+            image: 'https://construm.bugfinder.net/assets/uploads/content/6354e034159cc1666506804.png',
+            title: 'Awesome Team',
+        },
+        {
+            image: 'https://construm.bugfinder.net/assets/uploads/content/6354e042a6fd11666506818.png',
+            title: 'Quality Work',
+        },
+    ],
+    experienceDetails: [
+        { title: 'years of experience', value: '25' },
+        { title: 'Winning awards', value: '30' },
+        { title: 'completed projects', value: '55' },
+        { title: 'happy clients', value: '45' }
+    ],
+    pricingBackground: "https://construm.bugfinder.net/assets/uploads/content/63ad3183ef6ef1672294787.jpg",
+    experienceBackground: "https://construm.bugfinder.net/assets/uploads/content/63ad344d7d09e1672295501.jpg",
+};
